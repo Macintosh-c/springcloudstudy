c5b96b404882115c0b0688def018077a39405d8f
barry.cao <657582163@qq.com>
2019-09-12 17:05:27 +0800

    springcloud study first commit

---
+++ service-feign/src/main/java/com/jiang/clients/fallback/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package com.jiang.clients.fallback;
+
+import com.jiang.clients.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
---
+++ service-feign/src/main/java/com/jiang/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.jiang.controller;
+
+import com.jiang.clients.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @GetMapping(value = "/hi")
+    public String sayHi(@RequestParam String name) {
+        return schedualServiceHi.sayHiFromClientOne( name );
+    }
+}
